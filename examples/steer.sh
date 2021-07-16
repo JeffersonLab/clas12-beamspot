2c23428939a7b50a95dee7c5206ad0578f1316fc
Francesco Bossu <fbossu@jlab.org>
2021-07-16 07:41:30 -0400

    job example

---
+++ examples/steer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /group/clas12/packages/setup.sh
+module load clas12/pro
+
+BASEDIR=/w/hallb-scifs17exp/clas12/fbossu/beamspot/BeamSpot
+OUTDIR=$BASEDIR/output
+
+cp -v $BASEDIR/BeamSpot.java .
+cp -v $BASEDIR/run.sh .
+
+./run.sh -B 1 -O BS_$1 $2*$1*hipo
+
+mkdir -p $OUTDIR
+mv BS_* $OUTDIR
